4d5afaacb400236f0eb48e6a79d992d231e650fb
epotyugov <e.potyugov@gmail.com>
2022-02-03 00:11:01 +0300

    Добавлен install скрипт

---
+++ install.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$username = 'admin';
+$password = 'admin';
+$dbname = "db";
+$dsn = "mysql:host=$host;dbname=$dbname";
+
+try {
+	$connection = new PDO("mysql:host=$host", $username, $password);
+	$sql = file_get_contents("data/db.sql");
+	$connection->exec($sql);
+} catch (PDOException $error) {
+	echo 'провал: ' . $error;
+}
